54c4717944052b5285cd21be48e3e2542ffb04b3
Abed25 <officialwekesa254@gmail.com>
2025-03-19 00:29:07 +0300

    Fetch results

--- client/src/counsellors/pages/counsellor/counsellor.jsx
+++ client/src/counsellors/pages/counsellor/counsellor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../../styles/counsellor.css";
 import DynamicButton from "../../components/button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -10,6 +10,15 @@ import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
 
 export default function Counsellor() {
   const [expandedItems, setExpandedItems] = useState({}); // Stores which items are expanded
+  const [requests, setRequests] = useState([]);
+
+  // Fetch form data from API
+  useEffect(() => {
+    fetch("http://localhost:5000/api/submit-form")
+      .then((res) => res.json())
+      .then((data) => setRequests(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   const handleView = (id) => {
     setExpandedItems((prev) => ({
@@ -17,8 +26,6 @@ export default function Counsellor() {
       [id]: !prev[id], // Toggle the clicked item only
     }));
   };
-
-  const fakeMaps = [1, 2, 3, 4];
 
   const buttonStyle = {
     position: "absolute",
@@ -43,7 +50,7 @@ export default function Counsellor() {
       />
       <h2 style={{ textAlign: "center" }}>Student Requests</h2>
 
-      {fakeMaps.map((_, id) => (
+      {requests.map((request, id) => (
         <div
           key={id}
           className={
@@ -55,27 +62,12 @@ export default function Counsellor() {
             icon={expandedItems[id] ? faCircleChevronUp : faCircleChevronDown}
             onClick={() => handleView(id)}
           />
-          <h4>Title: Lost phone</h4>
-          <p>
-            ipsum dolor sit amet consectetur adipisicing elit. Iusto vel
-            repellat harum architecto eveniet, nemo similique obcaecati commodi
-            aliquid dignissimos dolorum ducimus quas corrupti quisquam
-            consequuntur culpa in aliquam neque. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Iusto vel repellat harum architecto
-            eveniet, nemo similique obcaecati commodi aliquid dignissimos
-            dolorum ducimus quas corrupti quisquam consequuntur culpa in aliquam
-            neque. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Iusto vel repellat harum architecto eveniet, nemo similique
-            obcaecati commodi aliquid dignissimos dolorum ducimus quas corrupti
-            quisquam consequuntur culpa in aliquam neque. Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Iusto vel repellat harum
-            architecto eveniet, nemo similique obcaecati commodi aliquid
-            dignissimos dolorum ducimus quas corrupti quisquam consequuntur
-            culpa in aliquam neque.
-          </p>
+          <h4>Name: {request.name}</h4>
+          <p>{request.email}</p>
+          <p>{request.message}</p>
           <div className="respondContainer">
             <form className="response">
-              <p> Give a response:</p>
+              <p>Give a response:</p>
               <textarea></textarea>
               <DynamicButton
                 name="Send"
