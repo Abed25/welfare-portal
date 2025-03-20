e6502ce19af4049194960e5650c9e841f1856791
Abed25 <officialwekesa254@gmail.com>
2025-03-21 01:27:55 +0300

    Landing page upgrade effect

--- client/src/general_pages/landing.jsx
+++ client/src/general_pages/landing.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import "../general_styles/landing.css";
 import Login from "./login";
 import SignUp from "./register";
+import { useNavigate } from "react-router-dom";
 export default function Landing() {
+  const navigate = useNavigate();
   return (
     <div className="landing">
       <div className="welcoming">
-        <h1 className="landing">Student welfare center</h1>
+        <section className="view1">
+          <h1>Student welfare portal</h1>
+          <button onClick={() => navigate("/signup")}>Get started</button>
+          <button onClick={() => navigate("/login")}>Sign in</button>
+        </section>
+        {/* <h1 className="landing">Student welfare center</h1> */}
       </div>
       <div className="auth">Pictures</div>
     </div>
