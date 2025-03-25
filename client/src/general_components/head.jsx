3ef576cd17d1675268b416f4913435082f67cacf
Abed25 <officialwekesa254@gmail.com>
2025-03-25 14:08:01 +0300

    User contact information

--- client/src/general_components/head.jsx
+++ client/src/general_components/head.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import "../general_styles/head.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { useAuth } from "../context/AuthProvider";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Head() {
+  const { user, role } = useAuth();
+  const [showContact, setShowContact] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  const ContactDetails = () => {
+    return (
+      <div className="contact">
+        <h4 style={{ textAlign: "center", textDecoration: "underline" }}>
+          Contact Information
+        </h4>
+        <p>Email: {user.email}</p>
+        <p>Role: {role}</p>
+        <button onClick={handleLogout}>Logout</button>
+      </div>
+    );
+  };
+
   return (
     <div className="head">
-      <label htmlFor="head">Head</label>
+      <label htmlFor="head">Welfare</label>
+      {user && (
+        <div className="contactContainer">
+          <FontAwesomeIcon
+            className={showContact ? "userIconActive" : "userIcon"}
+            icon={faUser}
+            onClick={() => setShowContact(!showContact)}
+            style={{ cursor: "pointer" }}
+          />
+          {showContact && <ContactDetails />}
+        </div>
+      )}
     </div>
   );
 }
