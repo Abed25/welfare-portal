4c8df6a7d0e6891f14f51dd7ecf9bd00fecd5122
Abed25 <officialwekesa254@gmail.com>
2025-03-26 14:17:16 +0300

    Flexible navigation

--- client/src/general_components/Navigation.jsx
+++ client/src/general_components/Navigation.jsx
@@ -1,33 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "../general_styles/Navigation.css";
 import HamburgerMenu from "./HamburgerMenu";
+import { useAuth } from "../context/AuthProvider";
 
-export default function Nav(props) {
+export default function Nav() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const { role } = useAuth();
+  const [userFunctions, setUserFunctions] = useState([]);
 
-  const handleClick = () => {
-    setMenuOpen(!menuOpen);
-  };
-  const studentFunctionalities = [
-    "Counselling",
-    "Marketplace",
-    "ToDO",
-    "Claim points",
-    "student portal",
-    "LMS",
-    "Exambank",
-  ];
+  useEffect(() => {
+    const functionalities = {
+      student: [
+        "Counselling",
+        "Marketplace",
+        "ToDO",
+        "Claim points",
+        "student portal",
+        "LMS",
+        "Exambank",
+      ],
+      counsellor: ["Feeds history", "ToDO", "Marketplace"],
+      default: ["Home", "About", "Login"],
+    };
+
+    setUserFunctions(functionalities[role] || functionalities.default);
+  }, [role]);
+
+  const handleClick = () => setMenuOpen(!menuOpen);
+
   return (
     <div className="nav">
       <ul className={menuOpen ? "view" : null}>
-        {studentFunctionalities.map((opt, id) => (
+        {userFunctions.map((opt, id) => (
           <NavLink key={id} to={`/${opt.toLowerCase()}`}>
             {opt}
           </NavLink>
         ))}
       </ul>
-
       <HamburgerMenu click={handleClick} />
     </div>
   );
