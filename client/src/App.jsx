fe8c7ceffcfbe7d8cc58937971146071587d4241
Abed25 <officialwekesa254@gmail.com>
2025-03-28 15:47:25 +0300

    Set toast container to be global

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "./layout";
 import { AuthProvider } from "./context/AuthProvider";
 import { WebSocketProvider } from "./context/WebSocketProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css"; // Import styles
 
 export default function App() {
   return (
@@ -9,6 +11,7 @@ export default function App() {
       <AuthProvider>
         <WebSocketProvider>
           <Layout />
+          <ToastContainer position="top-center" autoClose={3000} />
         </WebSocketProvider>
       </AuthProvider>
     </div>
