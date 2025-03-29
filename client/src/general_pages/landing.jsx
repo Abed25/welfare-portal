8320bfefd050916e80b0d06268a064373041a016
Abed25 <officialwekesa254@gmail.com>
2025-03-30 00:59:59 +0300

    Landing page upgrade

--- client/src/general_pages/landing.jsx
+++ client/src/general_pages/landing.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "../general_styles/landing.css";
 import { useNavigate } from "react-router-dom";
 import image1 from "../assets/system.avif";
+import image2 from "../assets/class.jpg";
 import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
+import { useAuth } from "../context/AuthProvider";
 export default function Landing() {
+  const { user, logout, role } = useAuth();
   const navigate = useNavigate();
   return (
     <div className="landing">
@@ -11,35 +14,58 @@ export default function Landing() {
         <section className="view1">
           <section className="view2">
             <h1>Student welfare portal</h1>
-            <button onClick={() => navigate("/signup")}>Get started</button>
-            <button onClick={() => navigate("/login")}>Sign in</button>
+
+            {user ? (
+              <div>
+                <button
+                  onClick={() =>
+                    navigate(
+                      role === "student"
+                        ? "/student-dashboard"
+                        : role === "counsellor"
+                        ? "/counsellor-dashboard"
+                        : "/login"
+                    )
+                  }
+                  className="landingBtns"
+                >
+                  Dashboard
+                </button>
+                <button onClick={logout} className="landingBtns">
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <div>
+                <button onClick={() => navigate()} className="landingBtns">
+                  Get started
+                </button>
+                <button
+                  onClick={() => navigate("/login")}
+                  className="landingBtns"
+                >
+                  Sign in
+                </button>
+              </div>
+            )}
           </section>
         </section>
         {/* <h1 className="landing">Student welfare center</h1> */}
       </div>
       <div className="auth">
-        {/* <img
-          src={image1}
-          alt="syystem image"
-          style={{ width: "50%", float: "left" }}
+        <img
+          src={image2}
+          alt="system image"
+          style={{
+            display: "block",
+            margin: "auto",
+            position: "absolute",
+            width: "90%",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
         />
-        <p>
-          A modern solution for managing student affairs efficiently. Access
-          welfare services, stay updated, and connect with support systems
-          seamlessly.
-        </p>
-        <h2>Why Use Our Portal?</h2>
-        <ul>
-          <li>📌 Easy access to student welfare services</li>
-          <li>📌 Secure and efficient management of student records</li>
-          <li>📌 Real-time updates and notifications</li>
-          <li>📌 Connect with student support teams effortlessly</li>
-        </ul>
-        <p>
-          Join thousands of students who are streamlining their welfare needs
-          through our platform. Register today and experience a hassle-free way
-          to stay connected!
-        </p> */}
       </div>
     </div>
   );
