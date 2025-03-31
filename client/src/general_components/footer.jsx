02e9d9c6722978a7d7475c48d502dc89d6d36a95
Abed25 <officialwekesa254@gmail.com>
2025-03-31 18:34:58 +0300

    superDev copyright protection

--- client/src/general_components/footer.jsx
+++ client/src/general_components/footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "../general_styles/footer.css";
 export default function Footer() {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">&copy; 2025 superDev. All rights reserved.</div>
+  );
 }
