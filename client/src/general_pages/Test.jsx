f2ed735e62c2c96fb1aeb8b16907345c2bce6d1c
Abed25 <officialwekesa254@gmail.com>
2025-04-04 23:50:18 +0300

    Testing message from websocket to client

--- client/src/general_pages/Test.jsx
+++ client/src/general_pages/Test.jsx
@@ -1,13 +1,41 @@
-import React from "react";
-import SubHeader from "../general_components/SubHeader";
-import Counsellor from "../counsellors/pages/counsellor/counsellor";
-import Counselling_Layout from "../counsellors/pages/layout";
+import React, { useContext } from "react";
+import { WebSocketContext } from "../context/WebSocketProvider";
+
 export default function Test() {
+  const { messages } = useContext(WebSocketContext);
+
   return (
     <div>
-      <SubHeader />
-      <Counselling_Layout />
-      <h1>Test page</h1>
+      <h1>Test Page</h1>
+      <h3>Received Messages:</h3>
+      <div>
+        {messages.length > 0 ? (
+          <ul>
+            {messages
+              .filter((msg) => msg.type === "studentReq") // ✅ Only studentReq messages
+              .map((msg, index) => (
+                <li key={index} style={{ color: "blue" }}>
+                  {" "}
+                  {msg.message}
+                </li>
+              ))}
+          </ul>
+        ) : (
+          <p>No messages received yet.</p>
+        )}
+      </div>
     </div>
   );
+}
+
+{
+  /* Receiving all messages body */
+}
+{
+  /* {messages.length > 0 ? (
+          messages.map((msg, index) => (
+            <pre key={index}>
+              📥 Received Data: {JSON.stringify(msg, null, 2)}
+            </pre>
+          )) */
 }
