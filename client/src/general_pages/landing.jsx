e51050b0f2f6d1d4b6254e8af26e18b8fb07cc85
Abed25 <officialwekesa254@gmail.com>
2025-04-06 20:15:08 +0300

    Upgraded landing page UI

--- client/src/general_pages/landing.jsx
+++ client/src/general_pages/landing.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../general_styles/landing.css";
 import { useNavigate } from "react-router-dom";
-import image1 from "../assets/system.avif";
-import image2 from "../assets/class.jpg";
-import { width } from "@fortawesome/free-brands-svg-icons/fa42Group";
 import { useAuth } from "../context/AuthProvider";
+
 export default function Landing() {
   const { user, logout, role } = useAuth();
   const navigate = useNavigate();
+  const [doneTyping, setDoneTyping] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDoneTyping(true), 4000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="landing">
       <div className="welcoming">
         <section className="view1">
           <section className="view2">
-            <h1>Student welfare portal</h1>
-
+            <h1>Student Welfare Portal</h1>
             {user ? (
               <div>
                 <button
@@ -37,7 +41,10 @@ export default function Landing() {
               </div>
             ) : (
               <div>
-                <button onClick={() => navigate()} className="landingBtns">
+                <button
+                  onClick={() => navigate("/register")}
+                  className="landingBtns"
+                >
                   Get started
                 </button>
                 <button
@@ -50,22 +57,15 @@ export default function Landing() {
             )}
           </section>
         </section>
-        {/* <h1 className="landing">Student welfare center</h1> */}
       </div>
+
       <div className="auth">
-        <img
-          src={image2}
-          alt="system image"
-          style={{
-            display: "block",
-            margin: "auto",
-            position: "absolute",
-            width: "90%",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-          }}
-        />
+        <div className={`typewriter-container ${doneTyping ? "typed" : ""}`}>
+          <span className="colorful-text">
+            {/* Behind this text i want a shining stars or sun */}
+            Empowering minds to shape the future.
+          </span>
+        </div>
       </div>
     </div>
   );
