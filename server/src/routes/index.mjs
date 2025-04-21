c8da06ffb9d62fa205cfbd132c4a40fea6b9d004
Abed25 <officialwekesa254@gmail.com>
2025-04-21 22:31:55 +0300

    Forum backend updates

--- server/src/routes/index.mjs
+++ server/src/routes/index.mjs
@@ -3,6 +3,7 @@ import formRouter from "./formRoutes.mjs";
 import helperRouter from "./healthCheck.mjs";
 import marketplaceRouter from "./marketplaceRoutes.mjs";
 import imageRouter from "./imageRoutes.mjs";
+import forumRoutes from "./ForumRoutes.mjs";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use(helperRouter);
 router.use(formRouter);
 router.use(marketplaceRouter);
 router.use(imageRouter);
+
+router.use("/api/forum", forumRoutes);
 
 export default router;
