9b16bd8df3c9aa4277b7f10369c0740f3f5e2004
Abed25 <officialwekesa254@gmail.com>
2025-04-29 14:43:05 +0300

    Display points and streak

--- client/src/points/pages/crypto.jsx
+++ client/src/points/pages/crypto.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import ClaimForm from "../components/claim";
+import Simulate from "../components/Simulate";
 
 export default function Crypto() {
   return (
     <div>
-      <h1>Comrades corner</h1>
+      <h1 style={{ textAlign: "center" }}>Comrades corner</h1>
+      <Simulate />
     </div>
   );
 }
